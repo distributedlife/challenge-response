151d063ce26351bc2f247b5bff8e2f421745f674
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-10-19 23:22:09 +0800

    change temporary effect to make better use of closures

--- inch/public/js/lib/util/temporary_effect.js
+++ inch/public/js/lib/util/temporary_effect.js
@@ -4,16 +4,17 @@ define([], function() {
 	return function(duration, on_tick_func) {
 		on_tick_func = on_tick_func || function() {};
 
+		var age = 0;
+		var progress = function() { 
+			return duration === 0 ? 0.0 : age / duration; 
+		}
+
 		return {
-			age: 0,
-
 			tick: function(dt) {
-				this.age += dt;
-				on_tick_func(dt, this.progress());
+				age += dt;
+				on_tick_func(dt, progress());
 			},
-
-			is_alive: function() { return (this.age < duration || duration === 0); },
-			progress: function() { return duration === 0 ? 0.0 : this.age / duration; }
+			is_alive: function() { return (age < duration || duration === 0); }
 		};
 	};
 });
