9b395e7878c909f16658033d5d26825772cc8e15
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-11-09 10:53:33 +0545

    pass in three.js rather than use package deps

--- game/js/practice.js
+++ game/js/practice.js
@@ -1,10 +1,11 @@
 "use strict";
 
 //TODO: this another way
+var THREE = require('inch-threejs');
 require("../../inch/public/js/font/helvetiker_regular");
 
 var configuration = {
-	camera: require('inch-orthographic-camera'),
+	camera: require('inch-orthographic-camera')(THREE),
 	display_config: {
 		controls: ['keyboard', 'gamepad']
 	},
