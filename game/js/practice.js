399bc3c52d3bf7358ff5f4ca73c8d68604cb7ea7
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-11-12 19:41:46 +0545

    removed input support as it's a module now

--- game/js/practice.js
+++ game/js/practice.js
@@ -1,14 +1,15 @@
 "use strict";
 
-//TODO: this another way
 var THREE = require('inch-threejs');
 var window = require('window');
+
+//TODO: this another way
 require("inch-font-helvetiker_regular")(THREE);
 
 var configuration = {
 	camera: require('inch-orthographic-camera')(THREE),
 	controls: [
-		require("../../inch/public/js/inch-rendering-engine/keyboard_controller")
+		require("inch-input-keyboard")
 	],
 	level: require("./levels/default")(THREE)
 };
