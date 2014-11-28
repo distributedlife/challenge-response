913b0e8f8a3246fb7511aeca5469952d7d0da8b4
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-11-28 21:07:55 +0530

    HTML overlay is now a custom jade file that is injected into the sweet spot via the template thingy

--- game.js
+++ game.js
@@ -4,6 +4,10 @@ var modes = {
     'practice': require("./game/js/modes/practice")
 };
 
+var overlays = {
+	'practice': process.cwd() + "/game/jade/primary.jade"
+}
+
 var routes = require('inch-routes-standard');
-var server = require("inch-express-server").Server("./game", routes(modes));
+var server = require("inch-express-server").Server("./game", routes(modes, overlays));
 server.start();
