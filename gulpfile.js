d0fff7d084cff822e01d5f4db4f6da6a9e79d7eb
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-11-29 23:12:35 +0530

    tidied up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,7 @@ var gulp = require('gulp');
 var server = require('gulp-develop-server');
 var livereload = require('gulp-livereload');
 var mocha = require('gulp-mocha');
-var jslint = require('gulp-jslint');
-var source = require('vinyl-source-stream');
-var browatchify = require('gulp-browatchify');
+var jshint = require('gulp-jshint');
 var reactify = require('reactify');
 var csslint = require('gulp-csslint');
 var sass = require('gulp-ruby-sass');
@@ -17,53 +15,65 @@ var del = require('del');
 var scsslint = require('gulp-scss-lint');
 var flatten = require('gulp-flatten');
 var browserify = require('gulp-browserify');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
 
 var paths = {
   js: ['game/**/*.js', 'game.js', '!game/js/gen/**'],
   scss: ['game/**/*.scss'],
   css: ['game/css'],
   tests: ['tests/**/*.js'],
-  genjs: ['game/js/gen/**']
+  genjs: './game/js/gen'
 };
 
+var onError = function (error) {
+    console.log(err);
+    this.emit('end');
+};
 
-gulp.task('delete-generated', function (cb) {
+gulp.task('delete-gen-css', function (cb) {
     del(paths.css, cb);
-    del(paths.genjs)
 });
-gulp.task('clean', ['delete-generated']);
+gulp.task('delete-gen-code', function (cb) {
+    del(paths.genjs, cb)
+});
+gulp.task('clean', ['delete-gen-css', 'delete-gen-code']);
 
 gulp.task('lint-code', function () {
     gulp.src(paths.js)
-        .pipe(jslint({
-            node: true,
-            vars: true,
-            nomen: true,
-            unparam: true
-        }));
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(jshint({
+            node: true
+        }))
+        .pipe(jshint.reporter('default', { verbose: true }));
 });
 gulp.task('lint-scss', function () {
     return gulp.src(paths.scss)
+        .pipe(plumber({errorHandler: onError}))
         .pipe(scsslint());
 });
 gulp.task('lint', ['lint-code', 'lint-scss']);
 
 gulp.task('test', function () {
     return gulp.src(paths.tests, {read: false})
+        .pipe(plumber({errorHandler: onError}))
         .pipe(mocha({reporter: 'nyan'}));
 });
 
 gulp.task('build-code', function() {
     gulp.src('./game/js/practice.js')
+        .pipe(plumber({errorHandler: onError}))
         .pipe(browserify({
-            debug: false
-            // transform: [reactify]
+            debug: false,
+            transform: [reactify]
         }))
-        .pipe(source('primary.js'))
-        .pipe(gulp.dest('./game/js/gen'));
+        .pipe(uglify())
+        .pipe(rename('primary.js'))
+        .pipe(gulp.dest(paths.genjs));
 });
 gulp.task('build-styles', function() {
     return gulp.src(paths.scss)
+        .pipe(plumber({errorHandler: onError}))
         .pipe(sass({ style: 'expanded', sourcemapPath: 'public/css' }))
         .pipe(autoprefixer({ cascade: false }))
         .pipe(rename({suffix: '.min'}))
@@ -73,34 +83,20 @@ gulp.task('build-styles', function() {
 });
 gulp.task('build', ['build-styles', 'build-code'])
 
-
 gulp.task('server:start', function () {
     server.listen({path: './game.js'});
 });
 
-// gulp.task('browatchify', function () {
-//     gulp.src('./game/js/practice.js')
-//         .pipe(browatchify({
-//             debug: !gulp.env.production,
-//             transforms: [reactify]
-//         }))
-//         .pipe(source('primary.js'))
-//         .pipe(gulp.dest('./game/js/gen'));
-// });
+gulp.task('watch', ['server:start'], function() {
+    function restart(file) {
+        server.changed(function (error) {
+            if (!error) { livereload.changed(file.path); }
+        });
+    };
 
-// gulp.task('server:restart', ['browatchify', 'server:start'], function () {
-//     function restart(file) {
-//         server.changed(function (error) {
-//             if (!error) { livereload.changed(file.path); }
-//         });
-//     }
-
-//     gulp.watch(['./game.js', "game/js/**/*.js", "inch/public/js/**/*.js"], { interval: 500 }, ['browatchify']).on('change', restart);
-// });
-
-gulp.task('watch', function() {
-  gulp.watch(paths.js, ['lint-code', 'test', 'build-code']);
-  gulp.watch(paths.scss, ['clean', 'lint-scss', 'build-styles']);
+    livereload.listen();
+    gulp.watch(paths.js, ['delete-gen-code', 'lint-code', 'test', 'build-code']).on('change', restart);
+    gulp.watch(paths.scss, ['delete-gen-css', 'lint-scss', 'build-styles']).on('change', restart);
 });
 
 gulp.task('default', ['clean', 'lint', 'test', 'build']);
