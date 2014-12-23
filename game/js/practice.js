a116c09490e6c9f36073a5fccd1aa474b9a30f3e
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-12-23 23:57:30 +1030

    note to self

--- game/js/practice.js
+++ game/js/practice.js
@@ -3,22 +3,22 @@
 var pluginManager = require('inch-plugins');
 pluginManager.set('Window', require('window'));
 pluginManager.set('GameMode', 'practice');
-pluginManager.set('AspectRatio', 26 / 10);
-pluginManager.set('WidescreenMinimumMargin', 32);
-pluginManager.set('Element', "canvas");
-pluginManager.set('FOV', 60);
-pluginManager.set('DebugProperties', {});
-pluginManager.load(require('inch-plugin-dimensions-widescreen'));
+pluginManager.set('AspectRatio', 26 / 10);									//default, required
+pluginManager.set('WidescreenMinimumMargin', 32);							//default, required
+pluginManager.set('Element', "canvas");										//default, required
+pluginManager.set('FOV', 60);												//default, required
+pluginManager.set('DebugProperties', {});									//default, required
+pluginManager.load(require('inch-plugin-dimensions-widescreen'));			//default, required
 pluginManager.load(require('inch-plugin-render-engine-adapter-threejs'));
-pluginManager.load(require('inch-plugin-connect-disconnect-behaviour'));
+pluginManager.load(require('inch-plugin-connect-disconnect-behaviour'));	//default, required
 pluginManager.load(require('inch-plugin-camera-orthographic-centred'));
 pluginManager.load(require('inch-plugin-input-mode-keyboard'));
 pluginManager.load(require("inch-plugin-debug-outside-in-grid"));
 pluginManager.load(require("inch-plugin-position-helper-2d"));
 pluginManager.load(require("inch-plugin-socket-behaviour-desktop"));
 pluginManager.load(require("inch-plugin-display-behaviour-standard"));
-pluginManager.load(require("inch-plugin-update-loop-vsync"));
-pluginManager.load(require('inch-plugin-icon-layout-fixed-aspect'));
+pluginManager.load(require("inch-plugin-update-loop-vsync"));				//default, required
+pluginManager.load(require('inch-plugin-icon-layout-fixed-aspect'));		//default, required
 pluginManager.load(require("inch-plugin-level-player-observer-count"));
 pluginManager.load(require('inch-plugin-level-standard-behaviour'));
 pluginManager.load(require("./levels/default"));
