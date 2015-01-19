dab0b6d32356e01c50fb42c4e7bc874a59497e02
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-01-19 22:14:17 +1000

    updated deps; updated ack map to use new format; make use of supplied state in ack map callback

--- game/js/entities/controller.js
+++ game/js/entities/controller.js
@@ -1,75 +1,64 @@
 "use strict";
 
-var _ = require('lodash');
-var updatableEntity = require('inch-entity-updatable');
-var sequence = require('inch-sequence');
+// module.exports = {
+//     delayed: function (controller) {
+//         controller = state.controller;
 
-module.exports = function (priorState) {
-    var controller = priorState || {};
-    _.defaults(controller, {
-        start: 0,
-        score: 0,
-        state: 'ready',
-        priorScores: []
-    });
+//         if (controller.state === 'falseStart') {
+//             return;
+//         }
 
-    var delayedEffects = require('inch-delayed-effects')();
-    var delayed = function () {
-        if (controller.state === 'falseStart') {
-            return;
-        }
+//         controller.state = "challengeStarted";
+//     },
+//     challengeSeen: function (ack, controller) {
+//         controller = state.controller;
 
-        controller.state = "challengeStarted";
-    };
+//         controller.start = ack.rcvdTimestamp;
+//     },
+//     rollUpAnUnnvervingDelay: function () {
+//         return Math.round(Math.random() * 6) + Math.round(Math.random() * 6);
+//     },
+//     response: function (force, data, controller) {
+//         controller = state.controller;
 
-    var rollUpAnUnnvervingDelay = function () {
-        return Math.round(Math.random() * 6) + Math.round(Math.random() * 6);
-    };
+//         if (controller.state === 'ready') {
+//             controller.state = "waiting";
+//             delayedEffects.add("pause-for-effect", this.rollUpAnUnnvervingDelay(), delayed);
+//             return;
+//         }
+//         if (controller.state === 'waiting') {
+//             controller.state = "falseStart";
+//             delayedEffects.cancelAll("pause-for-effect");
 
-    _.extend(controller, updatableEntity("controller", delayedEffects.update));
-    _.extend(controller, {
-        challengeSeen: function (ack) {
-            controller.start = ack.rcvdTimestamp;
-        },
-        response: function (force, data) {
-            if (controller.state === 'ready') {
-                controller.state = "waiting";
-                delayedEffects.add(rollUpAnUnnvervingDelay(), delayed);
-                return;
-            }
-            if (controller.state === 'waiting') {
-                controller.state = "falseStart";
-                delayedEffects.cancelAll();
+//             return;
+//         }
+//         if (controller.state === 'challengeStarted') {
+//             controller.state = 'complete';
+//             controller.score = data.rcvdTimestamp - controller.start;
+//             return;
+//         }
+//     },
+//     reset: function (controller) {
+//         controller = state.controller;
 
-                return;
-            }
-            if (controller.state === 'challengeStarted') {
-                controller.state = 'complete';
-                controller.score = data.rcvdTimestamp - controller.start;
-                return;
-            }
-        },
-        reset: function () {
-            if (controller.state === 'complete' || controller.state === "falseStart") {
+//         if (controller.state !== 'complete' && controller.state !== "falseStart") {
+//             return;
+//         }
 
-                if (controller.state === "falseStart") {
-                    this.priorScores.push({id: sequence.next("prior-scores"), score: "x"});
-                } else {
-                    this.priorScores.push({id: sequence.next("prior-scores"), score: controller.score});
+//         var score = controller.score;
+//         if (controller.state === "falseStart") {
+//             score = "x";
+//         }
 
-                    _.each(this.priorScores, function(priorScore) {
-                        priorScore.best = false;
-                    });
+//         controller.priorScores.push({id: sequence.next("prior-scores"), score: score});
+//         _.each(controller.priorScores, function(priorScore) {
+//             priorScore.best = false;
+//         });
 
-                    var bestScore = _.min(this.priorScores, function(priorScore) { return priorScore.score; });
-                    bestScore.best = true;
-                }
+//         var bestScore = _.min(controller.priorScores, function(priorScore) { return priorScore.score; });
+//         bestScore.best = true;
 
-                controller.score = 0;
-                controller.state = "ready";
-            }
-        }
-    });
-
-    return controller;
-};
+//         controller.score = 0;
+//         controller.state = "ready";
+//     }
+// };
--- game/js/modes/practice.js
+++ game/js/modes/practice.js
@@ -1,28 +1,107 @@
 "use strict";
 
-var entities = require('inch-entity-loader').loadFromPath(process.cwd() + "/game/js/entities/");
+//TODO: DO WE EVEN HAVE ENTITIES? OR DO WE JUST HAVE BEHAVIOUR?
+// var entities = require('inch-entity-loader').loadFromPath(process.cwd() + "/game/js/entities/");
+var controllerBehaviour = require(process.cwd() + '/game/js/entities/controller');
 var SocketSupport = require('inch-socket-support');
 var GameState = require('inch-game-state');
+var delayedEffects = require('inch-delayed-effects').DelayedEffects();
+var sequence = require('inch-sequence');
+var _ = require('lodash');
 
 module.exports = function (io) {
+    //TODO: define namespace when creating state
+    // var state = new GameState("distributedlife.challenge-response");
     var state = new GameState({
-        controller: new entities.controller()
+        controller: {
+            start: 0,
+            score: 0,
+            state: 'ready',
+            priorScores: []
+        }
     });
+
     //TODO: solve the namespace problem.
-    // state.addExternalModule(require('inch-player-observer-game-state'));
+    // state.addState("inch.core", state.core);                              //explicitly set the namespace and the initial state. This is used when?
+    // state.addStateFromModule(require('inch-game-state-player-observer')); //the module define the interface providing the namespace and the code itself
+
+    var delayed = function (controller) {
+        controller = state.controller;
+
+        if (controller.state === 'falseStart') {
+            return;
+        }
+
+        controller.state = "challengeStarted";
+    };
+
+    var challengeSeen = function (ack, controller) {
+        controller.start = ack.rcvdTimestamp;
+    };
+
+    var rollUpAnUnnvervingDelay = function () {
+        return Math.round(Math.random() * 6) + Math.round(Math.random() * 6);
+    };
+
+    var response = function (force, data, controller) {
+        controller = state.controller;
+
+        if (controller.state === 'ready') {
+            controller.state = "waiting";
+            delayedEffects.add("pause-for-effect", rollUpAnUnnvervingDelay(), delayed);
+            return;
+        }
+        if (controller.state === 'waiting') {
+            controller.state = "falseStart";
+            delayedEffects.cancelAll("pause-for-effect");
+
+            return;
+        }
+        if (controller.state === 'challengeStarted') {
+            controller.state = 'complete';
+            controller.score = data.rcvdTimestamp - controller.start;
+            return;
+        }
+    };
+
+    var reset = function (controller) {
+        controller = state.controller;
+
+        if (controller.state !== 'complete' && controller.state !== "falseStart") {
+            return;
+        }
+
+        var score = controller.score;
+        if (controller.state === "falseStart") {
+            score = "x";
+        }
+
+        controller.priorScores.push({id: sequence.next("prior-scores"), score: score});
+        _.each(controller.priorScores, function(priorScore) {
+            priorScore.best = false;
+        });
+
+        var bestScore = _.min(controller.priorScores, function(priorScore) { return priorScore.score; });
+        bestScore.best = true;
+
+        controller.score = 0;
+        controller.state = "ready";
+    };
+
+    //TODO: move controller logic to a behaviour class
 
     var actionMap = {
-        'space': [{target: state.controller.response, keypress: true, data: state.controller}],
-        'r': [{target: state.controller.reset.bind(state.controller), keypress: true}]
+        'space': [{target: response, keypress: true, data: state.controller}],
+        'r': [{target: reset, keypress: true, data: state.controller}]
     };
     var ackMap = {
-        'show-challenge': [state.controller.challengeSeen]
+        'show-challenge': [{target: challengeSeen, data: state.controller}]
     };
 
     var inputHandler = require('inch-input-handler').InputHandler(actionMap);
     var engine = require('inch-game-engine')(state.isPaused.bind(state), [
         inputHandler.update,
-        state.controller.update
+        delayedEffects.update
     ]);
     engine.run(120);
 
