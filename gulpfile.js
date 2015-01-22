a0a4cce77d88633305ae5c08ba10f388f53979ae
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-01-22 22:43:34 +1000

    fixed up scss warnings about appling styles to ids; don't uglify right now because it's so slow

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,7 @@ gulp.task('build-code', function() {
         .pipe(plumber({errorHandler: onError}))
         .pipe(browserified)
         .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(uglify())
+        // .pipe(uglify())
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(paths.genjs));
 });
