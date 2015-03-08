be5bddd893c0fc80314194755ac85b5c8d47f853
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-03-09 09:12:53 +1100

    tidy up alignment.js as it was breaking cyclomatic complexity rule

--- plugins/inch-geometry-alignment/src/alignment.js
+++ plugins/inch-geometry-alignment/src/alignment.js
@@ -2,38 +2,44 @@
 
 var _ = require('lodash');
 
+var repositionX = function (alignment, currentPosition, areaWidth) {
+  if (alignment === "left") {
+    return currentPosition.x;
+  } else if (alignment === "right") {
+    return currentPosition.x - areaWidth;
+  } else {
+    return currentPosition.x - (areaWidth / 2);
+  }
+};
+
+var repositionY = function (alignment, currentPosition, areaHeight) {
+  if (alignment === "top") {
+    return currentPosition.y + areaHeight;
+  } else if (alignment === "bottom") {
+    return currentPosition.y;
+  } else {
+    return currentPosition.y + (areaHeight / 4);
+  }
+};
+
 module.exports = {
-    toSelf2d: function (position, width, height, options) {
-        _.defaults(options, {
-            horizontal: "centre",
-            vertical: "centre"
-        });
+  toSelf2d: function (position, areaWidth, areaHeight, options) {
+    _.defaults(options, {
+      horizontal: "centre",
+      vertical: "centre"
+    });
 
-        var newPosition = {x: 0, y: 0, z: 0};
-        var resolvePositionValues = {
-            x: typeof position.x === 'function' ? position.x() : position.x,
-            y: typeof position.y === 'function' ? position.y() : position.y,
-            z: typeof position.z === 'function' ? position.z() : position.z
-        };
+    var newPosition = {x: 0, y: 0, z: 0};
+    var currentPosition = {
+      x: typeof position.x === 'function' ? position.x() : position.x,
+      y: typeof position.y === 'function' ? position.y() : position.y,
+      z: typeof position.z === 'function' ? position.z() : position.z
+    };
 
-        if (options.horizontal === "left") {
-            newPosition.x = resolvePositionValues.x;
-        } else if (options.horizontal === "right") {
-            newPosition.x = resolvePositionValues.x - width;
-        } else {
-            newPosition.x = resolvePositionValues.x - (width / 2);
-        }
+    newPosition.x = repositionX(options.horizontal, currentPosition, areaWidth);
+    newPosition.y = repositionY(options.vertical, currentPosition, areaHeight);
+    newPosition.z = currentPosition.z;
 
-        if (options.vertical === "top") {
-            newPosition.y = resolvePositionValues.y + height;
-        } else if (options.vertical === "bottom") {
-            newPosition.y = resolvePositionValues.y;
-        } else {
-            newPosition.y = resolvePositionValues.y + (height / 4);
-        }
-
-        newPosition.z = resolvePositionValues.z;
-
-        return newPosition;
-    }
+    return newPosition;
+  }
 };
