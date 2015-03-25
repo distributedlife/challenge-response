a0801d81336eec5b308d62dca0d0bbf48c2579c2
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-03-25 19:43:14 +1000

    pulled up functions that don't need to be within the scopre of start socket

--- plugins/inch-socket-support/src/socket-support.js
+++ plugins/inch-socket-support/src/socket-support.js
@@ -14,113 +14,113 @@ module.exports = {
     var io;
     var statistics = {};
 
-    return {
-      start: function (server, modeCallbacks) {
-        io = require('socket.io').listen(server);
+    var startUpdateClientLoop = function (id, socket) {
+      var lastPacket = {};
 
-        var startUpdateClientLoop = function (id, socket) {
-          var lastPacket = {};
-
-          var updateClient = function () {
-            var packet = {
-              gameState: RawStateAccess()
-            };
-
-            if (isEqual(packet.gameState, lastPacket.gameState)) {
-              return;
-            }
-
-            lastPacket = cloneDeep(packet);
-
-            packet.id = sequence.next('server-origin-messages');
-            packet.sentTimestamp = Date.now();
-            statistics[id].packets.unacked[packet.id] = packet.sentTimestamp;
-
-            socket.emit("gameState/update", packet);
-          };
-
-          setInterval(updateClient, 45);
+      var updateClient = function () {
+        var packet = {
+          gameState: RawStateAccess()
         };
 
-        var calculateLatency = function (id, pendingAcknowledgements) {
-          each(pendingAcknowledgements, function (ack) {
-            var sentTime = statistics[id].packets.unacked[ack.id];
+        if (isEqual(packet.gameState, lastPacket.gameState)) {
+          return;
+        }
 
-            statistics[id].latency.total += ack.rcvdTimestamp - sentTime;
+        lastPacket = cloneDeep(packet);
+
+        packet.id = sequence.next('server-origin-messages');
+        packet.sentTimestamp = Date.now();
+        statistics[id].packets.unacked[packet.id] = packet.sentTimestamp;
+
+        socket.emit("gameState/update", packet);
+      };
+
+      setInterval(updateClient, 45);
+    };
+
+    var calculateLatency = function (id, pendingAcknowledgements) {
+      each(pendingAcknowledgements, function (ack) {
+        var sentTime = statistics[id].packets.unacked[ack.id];
+
+        statistics[id].latency.total += ack.rcvdTimestamp - sentTime;
+      });
+
+      statistics[id].packets.totalAcked += size(pendingAcknowledgements);
+    };
+
+    var removeAcknowledgedPackets = function (id, pendingAcknowledgements) {
+      each(pendingAcknowledgements, function (ack) {
+        each(ack.names, function (name) {
+          if (AcknowledgementMap()[name] === undefined) { return; }
+
+          each(AcknowledgementMap()[name], function (action) {
+            StateMutator()(action.target(ack, action.data));
           });
+        });
 
-          statistics[id].packets.totalAcked += size(pendingAcknowledgements);
+        delete statistics[id].packets.unacked[ack.id];
+      });
+    };
+
+    var createOnInputFunction = function (id) {
+      return function (inputData) {
+        var pendingAcks = inputData.pendingAcks;
+        delete inputData.pendingAcks;
+
+        each(OnInput(), function (onInputCallback) {
+            onInputCallback(inputData, Date.now());
+        });
+
+        calculateLatency(id, pendingAcks);
+        removeAcknowledgedPackets(id, pendingAcks);
+      };
+    };
+
+    var createSetupPlayableClientFunction = function (modeCallback) {
+      return function (socket) {
+        var id = socket.id;
+
+        statistics[id] = {
+          packets: {
+            totalAcked: 0,
+            unacked: {}
+          },
+          latency: {
+            total: 0
+          }
         };
 
-        var removeAcknowledgedPackets = function (id, pendingAcknowledgements) {
-          each(pendingAcknowledgements, function (ack) {
-            each(ack.names, function (name) {
-              if (AcknowledgementMap()[name] === undefined) { return; }
+        modeCallback();
+        InitialiseState()();
 
-              each(AcknowledgementMap()[name], function (action) {
-                StateMutator()(action.target(ack, action.data));
-              });
-            });
+        var onInput = createOnInputFunction(id);
 
-            delete statistics[id].packets.unacked[ack.id];
-          });
-        };
-
-        var createOnInputFunction = function (id) {
-          return function (inputData) {
-            var pendingAcks = inputData.pendingAcks;
-            delete inputData.pendingAcks;
-
-            each(OnInput(), function (onInputCallback) {
-                onInputCallback(inputData, Date.now());
-            });
-
-            calculateLatency(id, pendingAcks);
-            removeAcknowledgedPackets(id, pendingAcks);
-          };
-        };
-
-        var createSetupPlayableClientFunction = function (modeCallback) {
-          return function (socket) {
-            var id = socket.id;
-
-            statistics[id] = {
-              packets: {
-                totalAcked: 0,
-                unacked: {}
-              },
-              latency: {
-                total: 0
-              }
-            };
-
-            modeCallback();
-            InitialiseState()();
-
-            var onInput = createOnInputFunction(id);
-
-            var mutateCallbackResponse = function (callbacks) {
-              return function() {
-                each(callbacks, function(callback) {
-                  StateMutator()(callback());
-                });
-              };
-            }
-
-            socket.on('disconnect', mutateCallbackResponse(OnPlayerDisconnect()));
-            socket.on('input', onInput);
-            socket.on('pause', mutateCallbackResponse(OnPause()));
-            socket.on('unpause', mutateCallbackResponse(OnUnpause()));
-
-            socket.emit("gameState/setup", RawStateAccess());
-
-            startUpdateClientLoop(id, socket);
-
-            each(OnPlayerConnect(), function(callback) {
+        var mutateCallbackResponse = function (callbacks) {
+          return function() {
+            each(callbacks, function(callback) {
               StateMutator()(callback());
             });
           };
-        };
+        }
+
+        socket.on('disconnect', mutateCallbackResponse(OnPlayerDisconnect()));
+        socket.on('input', onInput);
+        socket.on('pause', mutateCallbackResponse(OnPause()));
+        socket.on('unpause', mutateCallbackResponse(OnUnpause()));
+
+        socket.emit("gameState/setup", RawStateAccess());
+
+        startUpdateClientLoop(id, socket);
+
+        each(OnPlayerConnect(), function(callback) {
+          StateMutator()(callback());
+        });
+      };
+    };
+
+    return {
+      start: function (server, modeCallbacks) {
+        io = require('socket.io').listen(server);
 
         each(modeCallbacks, function(callback, mode) {
           io.of('/' + mode + '/primary').on('connection', createSetupPlayableClientFunction(callback));
