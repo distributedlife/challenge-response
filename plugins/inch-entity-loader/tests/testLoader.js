27cd322ded58e1aaba8014fc414ef9ff3e35a951
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-03-28 16:00:18 +1100

    fixed up the entity loader tests for their new home and to make them more async

--- plugins/inch-entity-loader/tests/testLoader.js
+++ plugins/inch-entity-loader/tests/testLoader.js
@@ -1,60 +1,72 @@
 var expect = require("expect");
+var async = require("async");
 var entityLoader = require('../src/loader.js');
-var fs = require("fs");
+var mkdir = require("fs").mkdir;
+var rmdir = require("fs").rmdir;
+var writeFile = require("fs").writeFile;
+var unlink = require("fs").unlink;
 
 describe('given an empty path', function(){
-	beforeEach(function() {
-		fs.mkdirSync('tests/empty');
+	beforeEach(function(done) {
+		mkdir(__dirname + '/empty', done);
 	});
 
-	afterEach(function() {
-		fs.rmdirSync('tests/empty');
+	afterEach(function(done) {
+		rmdir(__dirname + '/empty', done);
 	});
 
 	it('should return an empty hash', function(){
-		expect(entityLoader.loadFromPath('tests/empty')).toEqual({});
+		expect(entityLoader.loadFromPath('../tests/empty')).toEqual({});
 	});
 });
 
 describe('given a path with entities in it', function(){
-	beforeEach(function() {
-		fs.mkdirSync('tests/folder-of-things');
-		fs.writeFileSync('tests/folder-of-things/one.js', "module.exports = {herp: 'derp'}; ");
-		fs.writeFileSync('tests/folder-of-things/two.js', "module.exports = function() { return 'derp' };");
-		fs.writeFileSync('tests/folder-of-things/one.js.ignored', "The quick brown fox jumps over the lazy dog.");
+	beforeEach(function(done) {
+    async.series([
+      function (step) { mkdir(__dirname + '/folder', step); },
+      function (step) { writeFile(__dirname + '/folder/one.js', "module.exports = {herp: 'derp'}; ", step); },
+      function (step) { writeFile(__dirname + '/folder/two.js', "module.exports = function() { return 'derp' };", step); },
+      function (step) { writeFile(__dirname + '/folder/one.js.ignored', "The quick brown fox jumps over the lazy dog.", step); }
+    ], done);
 	});
 
-	afterEach(function() {
-		fs.unlinkSync('tests/folder-of-things/one.js')
-		fs.unlinkSync('tests/folder-of-things/two.js')
-		fs.unlinkSync('tests/folder-of-things/one.js.ignored')
-		fs.rmdirSync('tests/folder-of-things');
+	afterEach(function(done) {
+    async.series([
+  		function (step) { unlink(__dirname + '/folder/one.js', step); },
+  		function (step) { unlink(__dirname + '/folder/two.js', step); },
+  		function (step) { unlink(__dirname + '/folder/one.js.ignored', step); },
+  		function (step) { rmdir(__dirname + '/folder', step);; }
+    ], done);
 	});
 
 	it('should load each into the hash', function() {
-		var entities = entityLoader.loadFromPath('tests/folder-of-things');
+		var entities = entityLoader.loadFromPath('../tests/folder');
 		expect(entities.one).toEqual({herp: 'derp'});
 		expect(entities.two()).toEqual('derp');
 	});
 
 	it("should ignore files that don't end in .js", function() {
-		expect(entityLoader.loadFromPath('tests/folder-of-things/')['one.js.ignored']).toBe(undefined);
+		expect(entityLoader.loadFromPath('../tests/folder/')['one.js.ignored']).toBe(undefined);
 	});
 });
 
 describe('given an absolute path', function() {
-  beforeEach(function() {
-    fs.mkdirSync('tests/folder-of-things');
-    fs.writeFileSync('tests/folder-of-things/one.js', "module.exports = {herp: 'derp'}; ");
+  beforeEach(function(done) {
+    async.series([
+      function (step) { mkdir(__dirname + '/folder', step); },
+      function (step) { writeFile(__dirname + '/folder/one.js', "module.exports = {herp: 'derp'}; ", step); }
+    ], done);
   });
 
-  afterEach(function() {
-    fs.unlinkSync('tests/folder-of-things/one.js')
-    fs.rmdirSync('tests/folder-of-things');
+  afterEach(function(done) {
+    async.series([
+      function (step) { unlink(__dirname + '/folder/one.js', step); },
+      function (step) { rmdir(__dirname + '/folder', step); }
+    ], done);
   });
 
   it('should load each into the hash', function() {
-    var entities = entityLoader.loadFromPath(process.cwd() + '/tests/folder-of-things');
+    var entities = entityLoader.loadFromPath(__dirname + '/folder');
     expect(entities.one).toEqual({herp: 'derp'});
   });
 });
