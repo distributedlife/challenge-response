f139a4c7c26c4b00d651de47c3117ee8bb4003b9
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-04-03 14:18:16 +1100

    fix tests; jshint exclusions for test code

--- plugins/inch-game-engine/tests/engine_tests.js
+++ plugins/inch-game-engine/tests/engine_tests.js
@@ -1,28 +1,27 @@
-var expect = require('expect');
+'use strict';
+
 var assert = require('assert');
 var sinon = require('sinon');
 
 var deferDep = require('../../../tests/helpers.js').deferDep;
 
-var update = sinon.spy()
+var update = sinon.spy();
 var paused = false;
 var state = {
 	get: function () { return function() { return paused; }; }
-}
+};
 
-var Engine = require('../src/engine.js').func(deferDep([update]), deferDep(state));
-
-describe("the engine", function() {
-	var engine = null;
-	var clock = undefined;
-	var interval = undefined;
+describe('the engine', function() {
+	var engine;
+	var clock;
+	var interval;
 
 	beforeEach(function() {
 		update.reset();
 
 		clock = sinon.useFakeTimers();
 
-		engine = new Engine();
+		engine = require('../src/engine.js').func(deferDep([update]), deferDep(state));
 	});
 
 	afterEach(function() {
@@ -30,16 +29,16 @@ describe("the engine", function() {
 		clock.restore();
 	})
 
-	describe("when unpaused", function() {
-		it("should call each function passed in with the delta in ms", function() {
+	describe('when unpaused', function() {
+		it('should call each function passed in with the delta in ms', function() {
 			clock.tick(5000);
-			interval = engine.run(1);
+			interval = engine.run(0.5);
 			assert.deepEqual(update.firstCall.args, [5]);
 		});
 	});
 
-	describe("when paused", function() {
-		it("it should not call any update functions", function() {
+	describe('when paused', function() {
+		it('it should not call any update functions', function() {
 			paused = true;
 			interval = engine.run(1);
 			assert(!update.called);
--- plugins/inch-socket-support/tests/socket-support_test.js
+++ plugins/inch-socket-support/tests/socket-support_test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var expect = require('expect');
 var sinon = require('sinon');
 var _ = require('lodash');
@@ -16,9 +18,9 @@ var io = {
 			}
 		};
 	}
-}
+};
 var gameState = {
-	hi: "there"
+	hi: 'there'
 };
 
 var OnInput = sinon.spy();
@@ -29,28 +31,29 @@ var OnObserverDisconnect = sinon.spy();
 var OnPause = sinon.spy();
 var OnUnpause = sinon.spy();
 var StateMutator = sinon.spy();
-var InitialiseState = sinon.spy();
+var InitialiseState = {
+	initialise: sinon.spy()
+};
 
-var server = {}
+var server = {};
 
 var pendingAckCallback = sinon.spy();
 var ackMap = {
 	'first': [{target: pendingAckCallback, data: gameState}]
 };
-var mode = "arcade";
 var modeCallbacks = {
 	'arcade': sinon.spy()
 };
 
-var SocketSupport = require("../src/socket-support").func(deferDep(ackMap), deferDep([OnInput]), deferDep([OnPlayerConnect]), deferDep([OnPlayerDisconnect]), deferDep([OnObserverConnect]), deferDep([OnObserverDisconnect]), deferDep([OnPause]), deferDep([OnUnpause]), deferDep(gameState), deferDep(StateMutator), deferDep(InitialiseState));
+var SocketSupport = require('../src/socket-support').func(deferDep(ackMap), deferDep([OnInput]), deferDep([OnPlayerConnect]), deferDep([OnPlayerDisconnect]), deferDep([OnObserverConnect]), deferDep([OnObserverDisconnect]), deferDep([OnPause]), deferDep([OnUnpause]), deferDep(gameState), deferDep(StateMutator), deferDep(InitialiseState));
 
-describe("setting up the socket", function () {
+describe('setting up the socket', function () {
 	beforeEach(function () {
 		var socketIo = require('socket.io');
 		socketIo.listen = function(server) { return io; };
 
-		sinon.spy(io, "of");
-		sinon.spy(global, "setInterval");
+		sinon.spy(io, 'of');
+		sinon.spy(global, 'setInterval');
 		SocketSupport.start(server, modeCallbacks);
 	});
 
@@ -63,16 +66,18 @@ describe("setting up the socket", function () {
 		io.of.restore();
 	});
 
-	it("should listen on /:mode/primary", function () {
+	it('should listen on /:mode/primary', function () {
 		expect(io.of.calledOnce).toEqual(true);
-		expect(io.of.firstCall.args).toEqual(["/arcade/primary"]);
+		expect(io.of.firstCall.args).toEqual(['/arcade/primary']);
 	});
 });
 
-describe("on connect", function () {
+describe('on connect', function () {
+	var updateClientFunc;
+
 	beforeEach(function () {
 		socket.emit.reset();
-		sinon.spy(global, "setInterval");
+		sinon.spy(global, 'setInterval');
 		SocketSupport.start(server, modeCallbacks);
 
 		updateClientFunc = setInterval.firstCall.args[0];
@@ -87,34 +92,34 @@ describe("on connect", function () {
 	});
 
 
-	it("should setup the socket events", function () {
-		expect(socket.on.getCall(0).args[0]).toEqual("disconnect");
-		expect(socket.on.getCall(1).args[0]).toEqual("pause");
-		expect(socket.on.getCall(2).args[0]).toEqual("unpause");
-		expect(socket.on.getCall(3).args[0]).toEqual("input");
+	it('should setup the socket events', function () {
+		expect(socket.on.getCall(0).args[0]).toEqual('disconnect');
+		expect(socket.on.getCall(1).args[0]).toEqual('pause');
+		expect(socket.on.getCall(2).args[0]).toEqual('unpause');
+		expect(socket.on.getCall(3).args[0]).toEqual('input');
 	});
 
-	it("should send the initial game state to the client", function () {
-		expect(socket.emit.firstCall.args).toEqual(["gameState/setup", {hi: "there"}])
+	it('should send the initial game state to the client', function () {
+		expect(socket.emit.firstCall.args).toEqual(['gameState/setup', {hi: 'there'}]);
 	});
 
-	it("should start the update loop", function () {
+	it('should start the update loop', function () {
 		updateClientFunc();
 
-		expect(socket.emit.secondCall.args[0]).toEqual("gameState/update", {"hi": "there"});
+		expect(socket.emit.secondCall.args[0]).toEqual('gameState/update', {'hi': 'there'});
 	});
 
-	it("should call the onPlayerConnect callback", function () {
+	it('should call the onPlayerConnect callback', function () {
 		expect(OnPlayerConnect.called).toEqual(true);
 	});
 });
 
-describe("the client update loop", function () {
+describe('the client update loop', function () {
 	var updateClientFunc;
 
 	beforeEach(function () {
 		socket.emit.reset();
-		sinon.spy(global, "setInterval");
+		sinon.spy(global, 'setInterval');
 		SocketSupport.start(server, modeCallbacks);
 
 		updateClientFunc = setInterval.firstCall.args[0];
@@ -131,8 +136,8 @@ describe("the client update loop", function () {
 	it("should not send the packet if the game state hasn't changed", function () {
 		updateClientFunc();
 
-		expect(socket.emit.getCall(0).args[0]).toEqual("gameState/setup");
-		expect(socket.emit.getCall(1).args[0]).toEqual("gameState/update");
+		expect(socket.emit.getCall(0).args[0]).toEqual('gameState/setup');
+		expect(socket.emit.getCall(1).args[0]).toEqual('gameState/update');
 
 		socket.emit.reset();
 		updateClientFunc();
@@ -140,105 +145,105 @@ describe("the client update loop", function () {
 		expect(socket.emit.callCount).toEqual(0);
 	});
 
-	it("should send the packet if the game state has changed", function () {
+	it('should send the packet if the game state has changed', function () {
 		updateClientFunc();
 
-		expect(socket.emit.getCall(0).args[0]).toEqual("gameState/setup");
-		expect(socket.emit.getCall(1).args[0]).toEqual("gameState/update");
+		expect(socket.emit.getCall(0).args[0]).toEqual('gameState/setup');
+		expect(socket.emit.getCall(1).args[0]).toEqual('gameState/update');
 
 		socket.emit.reset();
 		gameState.altered = true;
 		updateClientFunc();
 
 		expect(socket.emit.callCount).toEqual(1);
-		expect(socket.emit.firstCall.args[0]).toEqual("gameState/update");
+		expect(socket.emit.firstCall.args[0]).toEqual('gameState/update');
 	});
 
-	it("should give each packet an id", function () {
+	it('should give each packet an id', function () {
 		updateClientFunc();
 
-		expect(socket.emit.secondCall.args[0]).toEqual("gameState/update");
+		expect(socket.emit.secondCall.args[0]).toEqual('gameState/update');
 		expect(socket.emit.secondCall.args[1].id).toEqual(5);
 	});
 
-	it("should record the sent time of each packet", function () {
+	it('should record the sent time of each packet', function () {
 		updateClientFunc();
 
 		expect(socket.emit.secondCall.args[1].sentTimestamp).toNotEqual(undefined);
 	});
 
-	it("should send game state about every 45ms", function() {
+	it('should send game state about every 45ms', function() {
 		expect(setInterval.firstCall.args[1]).toEqual(45);
 	});
 });
 
-describe("on input", function () {
+describe('on input', function () {
 	var inputData;
 
 	beforeEach(function () {
 		inputData = {
-			pendingAcks: [{names: ["first"], id: 1}]
+			pendingAcks: [{names: ['first'], id: 1}]
 		};
 
 		pendingAckCallback.reset();
 
-		expect(socket.on.getCall(3).args[0]).toEqual("input");
+		expect(socket.on.getCall(3).args[0]).toEqual('input');
 		socket.on.getCall(3).args[1](inputData);
 	});
 
-	it("should remove pending acks from the input data", function () {
+	it('should remove pending acks from the input data', function () {
 		expect(inputData.pendingAcks).toEqual(undefined);
 	});
 
-	it.skip("should calculate the latency of each packet");
+	it.skip('should calculate the latency of each packet');
 	it.skip("remove ack'd packets from the unacked packet list");
 
-	it("should call the onNewUserInput callback", function () {
+	it('should call the onNewUserInput callback', function () {
 		expect(OnInput.called).toEqual(true);
 	});
 
-	it("should call the pendingAck target", function () {
+	it('should call the pendingAck target', function () {
 		expect(pendingAckCallback.called).toEqual(true);
 	});
 
-	it("should pass the ack into the pendingAck target", function () {
-		expect(pendingAckCallback.firstCall.args[0]).toEqual({names: ["first"], id: 1});
+	it('should pass the ack into the pendingAck target', function () {
+		expect(pendingAckCallback.firstCall.args[0]).toEqual({names: ['first'], id: 1});
 	});
 
-	it("should pass the data parameter into the pendingAck target", function () {
+	it('should pass the data parameter into the pendingAck target', function () {
 		expect(pendingAckCallback.firstCall.args[1]).toEqual(gameState);
 	});
 });
 
-describe("on pause", function () {
+describe('on pause', function () {
 	beforeEach(function () {
-		expect(socket.on.getCall(1).args[0]).toEqual("pause");
+		expect(socket.on.getCall(1).args[0]).toEqual('pause');
 		socket.on.getCall(1).args[1]();
 	});
 
-	it("should call the onPause callback", function() {
+	it('should call the onPause callback', function() {
 		expect(OnPause.calledOnce).toEqual(true);
 	});
 });
 
-describe("on unpause", function () {
+describe('on unpause', function () {
 	beforeEach(function () {
-		expect(socket.on.getCall(2).args[0]).toEqual("unpause");
+		expect(socket.on.getCall(2).args[0]).toEqual('unpause');
 		socket.on.getCall(2).args[1]();
 	});
 
-	it("should call the onUnpause callback", function () {
+	it('should call the onUnpause callback', function () {
 		expect(OnUnpause.calledOnce).toEqual(true);
 	});
 });
 
-describe("on disconnect", function () {
+describe('on disconnect', function () {
 	beforeEach(function () {
-		expect(socket.on.getCall(0).args[0]).toEqual("disconnect");
+		expect(socket.on.getCall(0).args[0]).toEqual('disconnect');
 		socket.on.getCall(0).args[1]();
 	});
 
-	it("should call the onPlayerDisonnect callback", function () {
+	it('should call the onPlayerDisonnect callback', function () {
 		expect(OnPlayerDisconnect.calledOnce).toEqual(true);
 	});
 });
