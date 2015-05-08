0a8fd15ffdf4a476e538a642d62fb65f79b7b409
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-05-08 10:29:45 +1000

    now using the new view plugin

--- game/js/easy.js
+++ game/js/easy.js
@@ -3,6 +3,7 @@
 var entryPoint = require('ensemblejs-client');
 entryPoint.loadWindow(require('window'));
 entryPoint.loadDefaults();
+entryPoint.set('GameMode', 'easy');
 entryPoint.load(require('./views/easy'));
 entryPoint.load(require('./events/on-mute'));
 entryPoint.load(require('./events/on-unmute'));
--- game/js/views/easy.js
+++ game/js/views/easy.js
@@ -1,13 +1,14 @@
 'use strict';
 
+//jshint maxparams:7
 module.exports = {
-  deps: ['Element', 'StateTrackerHelpers', 'Dimensions', 'StateTracker'],
+  deps: ['Element', 'StateTrackerHelpers', 'StateTracker', 'PendingAcknowledgements', 'RegisterEffect', 'DefinePlugin'],
   type: 'View',
-  func: function (element, trackerHelpers, dimensions, tracker) {
+  func: function (element, trackerHelpers, tracker, pendingAcknowledgements, registerEffect, define) {
     var colour = require('color');
     var Howl = require('howler').Howl;
     var $ = require('zepto-browserify').$;
-    var _ = require('lodash');
+    var each = require('lodash').each;
     var equals = trackerHelpers().equals;
     var centreInCamera = require('../../../supporting-libs/src/centre-in-camera');
     var adapter = require('../../../three-js-dep/inch-plugin-render-engine-adapter-threejs/src/adapter');
@@ -15,134 +16,135 @@ module.exports = {
     var theCamera = require('../../../three-js-dep/inch-plugin-camera-orthographic-centred/src/camera');
     var mainTemplate = require('../../views/overlays/easy.jade');
     var priorScoresTemplate = require('../../views/partials/priorScores.jade');
-
+    var dims;
     var camera;
     var renderer;
     var scene;
-    return {
-      screenResized: function (dims) {
-        if (renderer) {
-          renderer.setSize(dims.usableWidth, dims.usableHeight);
-        }
 
-        if (camera) {
-          adapter.setCameraAspectRatio(camera, dims.ratio);
+    return function (newDims) {
+      dims = newDims;
+
+      var Circle = require('../../../three-js-dep/inch-geometry2d-circle/src/circle.js')(adapter);
+
+      $('#overlay').append(mainTemplate());
+
+
+      //Render layer concern
+      //Setup threejs-camera, inch-scene, threejs-scene, threejs-renderer
+      camera = theCamera(adapter, dims);
+      scene = require('../../../three-js-dep/inch-scene/src/scene.js')(adapter.createScene());
+      renderer = adapter.createRenderer(dims);
+      adapter.attachRenderer(element(), renderer);
+
+      scene.add(grid(adapter, dims));
+
+      var showInstructions = function (model, priorModel, statusIndicator) {
+        $('#instructions').show();
+        $('#challenge').hide();
+        $('#results').hide();
+        $('#falsestart').hide();
+        statusIndicator.changeColour(0, colour('grey').rgbArray());
+      };
+
+      var hideInstructions = function (model, priorModel, statusIndicator, waitingSound) {
+        $('#instructions').hide();
+        statusIndicator.changeColour(0, colour('red').rgbArray());
+        waitingSound.play();
+      };
+
+      var showChallenge = function (model, priorModel, statusIndicator, goSound, waitingSound) {
+        waitingSound.stop();
+        goSound.play();
+
+        $('#challenge').show();
+        pendingAcknowledgements().ackLast('show-challenge');
+        statusIndicator.changeColour(0, colour('green').rgbArray());
+      };
+
+      var showResults = function (model, priorModel, statusIndicator) {
+        $('#challenge').hide();
+        $('#results').show();
+        statusIndicator.changeColour(0, colour('black').rgbArray());
+      };
+
+      var showFalseStart = function (model, priorModel, statusIndicator, goSound, waitingSound) {
+        $('#falsestart').show();
+        statusIndicator.changeColour(0, colour('orange').rgbArray());
+        goSound.stop();
+        waitingSound.stop();
+      };
+
+      var updateScore = function (model) {
+        $('#score')[0].innerText = model;
+
+        var score = $('#score');
+        var centered = centreInCamera(dims, camera, score.width(), score.height());
+
+        score.css('left', centered.left + 'px');
+        score.css('top', centered.top + 'px');
+        score.show();
+      };
+
+      var statusIndicator = new Circle(scene.add, scene.remove, {
+        radius: 100,
+        segments: 80,
+        position: { x: 0, y: 0, z: -100}
+      });
+      registerEffect().register(statusIndicator);
+
+      var waitingSound = new Howl({
+        urls: ['/game/audio/waiting.mp3']
+      });
+      var goSound = new Howl({
+        urls: ['/game/audio/go.mp3']
+      });
+
+      var onScoreAddedFunction = function() {
+        return function (currentValue) {
+          $('#prior-scores').append(priorScoresTemplate({id: 'prior-score-' + currentValue.id, score: currentValue.score}));
+        };
+      };
+      var updateHightlight = function(currentValue) {
+        if (currentValue.best) {
+          $('#prior-score-' + currentValue.id).addClass('best');
+        } else {
+          $('#prior-score-' + currentValue.id).removeClass('best');
         }
-      },
-      update: function() {
-        if (renderer) {
+      };
+      var addExistingScoresFunction = function() {
+        return function (currentValues) {
+          each(currentValues, function(value) {
+            $('#prior-scores').append(priorScoresTemplate({id: 'prior-score-' + value.id, score: value.score}));
+          });
+        };
+      };
+
+      var theGameState = function (state) { return state.controller.state; };
+      var theScore = function (state) { return state.controller.score; };
+      var thePriorScores = function (state) { return state.controller.priorScores; };
+
+      tracker().onChangeTo(theGameState, equals('ready'), showInstructions, statusIndicator);
+      tracker().onChangeTo(theGameState, equals('waiting'), hideInstructions, [statusIndicator, waitingSound]);
+      tracker().onChangeTo(theGameState, equals('challengeStarted'), showChallenge, [statusIndicator, goSound, waitingSound]);
+      tracker().onChangeTo(theGameState, equals('complete'), showResults, statusIndicator);
+      tracker().onChangeTo(theGameState, equals('falseStart'), showFalseStart, [statusIndicator, goSound, waitingSound]);
+      tracker().onChangeOf(theScore, updateScore);
+      tracker().onElementAdded(thePriorScores, onScoreAddedFunction(), addExistingScoresFunction());
+      tracker().onElementChanged(thePriorScores, updateHightlight);
+
+      define()('OnEachFrame', function () {
+        return function () {
           renderer.render(scene.scene(), camera);
-        }
-      },
-      setup: function (ackLastRequest, register) {
-        var Circle = require('../../../three-js-dep/inch-geometry2d-circle/src/circle.js')(adapter);
-
-        $('#overlay').append(mainTemplate());
-
-
-        //Render layer concern
-        //Setup threejs-camera, inch-scene, threejs-scene, threejs-renderer
-        camera = theCamera(adapter, dimensions().get());
-        scene = require('../../../three-js-dep/inch-scene/src/scene.js')(adapter.createScene());
-        renderer = adapter.createRenderer(dimensions().get());
-        adapter.attachRenderer(element(), renderer);
-
-        scene.add(grid(adapter, dimensions().get()));
-
-        var showInstructions = function (model, priorModel, statusIndicator) {
-          $('#instructions').show();
-          $('#challenge').hide();
-          $('#results').hide();
-          $('#falsestart').hide();
-          statusIndicator.changeColour(0, colour('grey').rgbArray());
         };
+      });
+      define()('OnResize', function () {
+        return function (newDims) {
+          dims = newDims;
 
-        var hideInstructions = function (model, priorModel, statusIndicator, waitingSound) {
-          $('#instructions').hide();
-          statusIndicator.changeColour(0, colour('red').rgbArray());
-          waitingSound.play();
+          renderer.setSize(dims.usableWidth, dims.usableHeight);
+          adapter.setCameraAspectRatio(camera, dims.ratio);
         };
-
-        var showChallenge = function (model, priorModel, statusIndicator, goSound, waitingSound) {
-          waitingSound.stop();
-          goSound.play();
-
-          $('#challenge').show();
-          ackLastRequest('show-challenge');
-          statusIndicator.changeColour(0, colour('green').rgbArray());
-        };
-
-        var showResults = function (model, priorModel, statusIndicator) {
-          $('#challenge').hide();
-          $('#results').show();
-          statusIndicator.changeColour(0, colour('black').rgbArray());
-        };
-
-        var showFalseStart = function (model, priorModel, statusIndicator, goSound, waitingSound) {
-          $('#falsestart').show();
-          statusIndicator.changeColour(0, colour('orange').rgbArray());
-          goSound.stop();
-          waitingSound.stop();
-        };
-
-        var updateScore = function (model) {
-          $('#score')[0].innerText = model;
-
-          var score = $('#score');
-          var centered = centreInCamera(dimensions().get(), camera, score.width(), score.height());
-
-          score.css('left', centered.left + 'px');
-          score.css('top', centered.top + 'px');
-          score.show();
-        };
-
-        var statusIndicator = new Circle(scene.add, scene.remove, {
-          radius: 100,
-          segments: 80,
-          position: { x: 0, y: 0, z: -100}
-        });
-        register(statusIndicator);
-
-        var waitingSound = new Howl({
-          urls: ['/game/audio/waiting.mp3']
-        });
-        var goSound = new Howl({
-          urls: ['/game/audio/go.mp3']
-        });
-
-        var onScoreAddedFunction = function() {
-          return function (currentValue) {
-            $('#prior-scores').append(priorScoresTemplate({id: 'prior-score-' + currentValue.id, score: currentValue.score}));
-          };
-        };
-        var updateHightlight = function(currentValue) {
-          if (currentValue.best) {
-            $('#prior-score-' + currentValue.id).addClass('best');
-          } else {
-            $('#prior-score-' + currentValue.id).removeClass('best');
-          }
-        };
-        var addExistingScoresFunction = function() {
-          return function (currentValues) {
-            _.each(currentValues, function(value) {
-              $('#prior-scores').append(priorScoresTemplate({id: 'prior-score-' + value.id, score: value.score}));
-            });
-          };
-        };
-
-        var theGameState = function (state) { return state.controller.state; };
-        var theScore = function (state) { return state.controller.score; };
-        var thePriorScores = function (state) { return state.controller.priorScores; };
-
-        tracker().onChangeTo(theGameState, equals('ready'), showInstructions, statusIndicator);
-        tracker().onChangeTo(theGameState, equals('waiting'), hideInstructions, [statusIndicator, waitingSound]);
-        tracker().onChangeTo(theGameState, equals('challengeStarted'), showChallenge, [statusIndicator, goSound, waitingSound]);
-        tracker().onChangeTo(theGameState, equals('complete'), showResults, statusIndicator);
-        tracker().onChangeTo(theGameState, equals('falseStart'), showFalseStart, [statusIndicator, goSound, waitingSound]);
-        tracker().onChangeOf(theScore, updateScore);
-        tracker().onElementAdded(thePriorScores, onScoreAddedFunction(), addExistingScoresFunction());
-        tracker().onElementChanged(thePriorScores, updateHightlight);
-      }
+      });
     };
   }
 };
