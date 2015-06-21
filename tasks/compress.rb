dccfe22f90eeba1467c32abe16d945942ebbd8f4
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-06-21 23:44:06 +1000

    remove gzip from build; supported by server now

--- tasks/compress.rb
+++ tasks/compress.rb
@@ -16,13 +16,3 @@ def minify files
     cp f, min_name
   end
 end
-
-def gzip files
-  files.each do |f|
-    min_name = f.gsub(".js", ".min.js")
-
-    gzip = "gzip -9k #{min_name}"
-    puts gzip
-    system "#{gzip}"
-  end
-end
